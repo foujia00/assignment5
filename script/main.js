854d15769afe250fa5cb07905d35d54109536606
foujia00 <ftprotiva@gmail.com>
2025-03-06 21:22:11 +0600

    2nd commit

---
+++ script/main.js
@@ -0,0 +1,75 @@
+document.getElementById("discover-new").addEventListener("click",function(event) {
+    window.location.href="./que.html"
+})
+document.getElementById("cards-1").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+
+    this.disabled=true;
+})
+document.getElementById("cards-2").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+    
+    this.disabled=true;
+})
+document.getElementById("cards-3").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+    
+    this.disabled=true;
+})
+document.getElementById("cards-4").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+    
+    this.disabled=true;
+})
+document.getElementById("cards-5").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+    
+    this.disabled=true;
+})
+document.getElementById("cards-6").addEventListener("click",function(event) {
+    const taskAssigned = document.getElementById("task-assigned").innerText;
+    const convertedTaskAssigned = parseFloat(taskAssigned);
+    const sub = convertedTaskAssigned - 1 ;
+    document.getElementById("task-assigned").innerText=sub;
+    const totalAssign = document.getElementById("total-assign").innerText;
+    const convertedTotalkAssign = parseFloat(totalAssign);
+    const sum = convertedTotalkAssign + 1 ;
+    document.getElementById("total-assign").innerText = sum ;
+    
+    this.disabled=true;
+})
